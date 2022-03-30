885680bcacef0b9ca879fbedf3095020e8a38b43
Indiano Djons <101926857+IndianoDjons@users.noreply.github.com>
2022-03-30 10:36:35 +0300

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,42 @@
+import React, {useState} from 'react';
+import './App.css';
+
+
+function App() {
+
+    return (
+        <div className="App">
+            <Todolist/>
+            <Todolist/>
+            <Todolist/>
+        </div>
+    );
+}
+
+
+function Todolist() {
+
+    return (
+        <div>
+            <h3>What to Learn</h3>
+            <div>
+                <input/>
+                <button>+</button>
+            </div>
+            <ul>
+                <li><input type="checkbox" checked={true}/><span>CSS&HTML</span></li>
+                <li><input type="checkbox" checked={true}/><span>JS</span></li>
+                <li><input type="checkbox" checked={false}/><span>React</span></li>
+            </ul>
+            <div>
+                <button>All</button>
+                <button>Active</button>
+                <button>Completed</button>
+            </div>
+        </div>
+    )
+}
+
+
+export default App;
+
